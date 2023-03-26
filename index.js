91ddabae283350c951eeeee0965be689d0a6ab8a
Benah0987 <88454033+Benah0987@users.noreply.github.com>
2023-03-26 21:40:51 +0300

    Update index.js
    completed assignment, added the three functions

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
-}
+  //if the distance is less than 400, it should say 'thts on me'
+    if (distance <= 400) {
+      return 'This one is on me!';
+  //using the else if, if the distance is greater than 2500 it returns 'no can do' 
+    } else if (distance > 2500) {
+      return 'No can do.';
+  //this rep the distance over 2000 feet
+    } else {
+      return 'I will gladly take your thirty bucks.';
+    }
+  }
+  scuberGreetingForFeet(1800                     )
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  
 
-function switchOnCharmFromTip(){
+
+function ternaryCheckCity(city){
   // Write your code here!
+ //the ternary operator uses the '?' to know if its NYC using the '===' strict equality 
+ //operator ':' this separates the choices to be used
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  
 }
+console.log(ternaryCheckCity(NYC))
+
+
+  // Write your code here!
+  function switchOnCharmFromTip(tip) {
+    //i used the switch statement here
+    //the tip parameter is passed as the value being evaluated
+    switch (tip) {
+      //each case statements checks if the expression matches 
+      case 'generous':
+        return 'Thank you so much.';
+      case 'not as generous':
+        return 'Thank you.';
+    //the default value executes if no tip was good   
+      default:
+        return 'Bye.';
+    }
+  }
+  
+    
+  
